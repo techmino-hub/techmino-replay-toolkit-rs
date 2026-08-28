4a61191e4679ce025b9f11a81020d1baf99481a4
Not-A-Normal-Robot <not_a_normal_robot@protonmail.com>
2026-08-29 00:58:47 +0700

    feat(trt): Work on backend inspection operations

--- src/backend/fetch.rs
+++ src/backend/fetch.rs
@@ -1,24 +1,120 @@
 //! Free functions for fetching metadata and input event data from a given
 //! replay file.
 
-use std::path::Path;
+use std::{
+    fs::File,
+    io::{self, BufReader, prelude::BufRead},
+    path::Path,
+};
 
-use libtechmino_replay::replay::{GameInputEvent, GameReplayMetadata};
+use libtechmino_replay::{
+    consts::METADATA_EVENTDATA_SEPARATOR,
+    deserialize::{Decoded, ReplayDecoder, ReplayDecoderPreprocessor},
+    errors::ReplayParseError,
+    format::ReplayBufferKind,
+    replay::{GameInputEvent, GameReplayMetadata},
+};
 
 use crate::ParseOrIoError;
 
 /// Reads the metadata from a replay file.
 ///
 /// # Errors
-/// Returns an error if something went wrong while
+/// Returns an error if something went wrong while extracting the metadata.
 pub(in crate::backend) fn fetch_metadata(
     path: &Path,
 ) -> Result<GameReplayMetadata, ParseOrIoError> {
-    todo!();
+    let file = File::open(path)?;
+    let mut file = BufReader::new(file);
+
+    let first_chunk = file.fill_buf()?;
+    let first_byte = first_chunk.first().ok_or(ReplayParseError::UnexpectedEnd)?;
+    let replay_kind = ReplayBufferKind::try_from_first_byte(*first_byte)
+        .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, format!("{e}")))?;
+
+    let mut processed = Vec::new();
+    let mut newly_appended: &[u8] = &[];
+
+    let mut preprocessor = ReplayDecoderPreprocessor::new(replay_kind);
+
+    while !newly_appended.contains(&METADATA_EVENTDATA_SEPARATOR) {
+        let input_buf = file.fill_buf()?;
+
+        if input_buf.is_empty() {
+            break;
+        }
+
+        let input_len = input_buf.len();
+
+        let old_output_len = processed.len();
+        preprocessor
+            .preprocess(input_buf, &mut processed)
+            .map_err(ReplayParseError::from)?;
+
+        file.consume(input_len);
+        newly_appended = &processed[old_output_len..];
+    }
+
+    let separator_pos = processed
+        .iter()
+        .position(|byte| *byte == METADATA_EVENTDATA_SEPARATOR)
+        .ok_or(ReplayParseError::MetadataSeparatorNotFound)?;
+    let metadata_bytes = &processed[..separator_pos];
+
+    debug_assert!(
+        !metadata_bytes.contains(&METADATA_EVENTDATA_SEPARATOR),
+        "Metadata bytes should not have the metadata-eventdata separator"
+    );
+
+    let metadata: GameReplayMetadata = serde_json::from_slice(metadata_bytes)
+        .map_err(ReplayParseError::MetadataDeserializeError)?;
+
+    Ok(metadata)
 }
 
+/// Reads the input event data from a replay file.
+///
+/// # Errors
+/// Returns an error if something went wrong while extracting the input event
+/// data.
 pub(in crate::backend) fn fetch_input_data(
     path: &Path,
 ) -> Result<Vec<GameInputEvent>, ParseOrIoError> {
-    todo!();
+    let file = File::open(path)?;
+
+    let mut file = BufReader::new(file);
+
+    let first_chunk = file.fill_buf()?;
+    let first_byte = first_chunk.first().ok_or(ReplayParseError::UnexpectedEnd)?;
+    let replay_kind = ReplayBufferKind::try_from_first_byte(*first_byte)
+        .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, format!("{e}")))?;
+
+    let mut decoder = ReplayDecoder::new(replay_kind, None);
+    let mut events: Vec<GameInputEvent> = Vec::new();
+
+    loop {
+        let in_buf = file.fill_buf()?;
+
+        if in_buf.is_empty() {
+            break;
+        }
+
+        let in_len = in_buf.len();
+
+        let Decoded {
+            metadata: _,
+            inputs: new_evs,
+            ..
+        } = decoder.update(in_buf)?;
+
+        events.copy_from_slice(&new_evs);
+
+        file.consume(in_len);
+    }
+
+    if !decoder.is_finished() {
+        return Err(ReplayParseError::UnexpectedEnd.into());
+    }
+
+    Ok(events)
 }
